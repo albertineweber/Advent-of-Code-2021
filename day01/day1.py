c0ec7ba5573c837807f4df34d3a05911bf8df5e5
Albertine <albertine.wc@gmail.com>
2021-12-04 19:19:46 -0300

    add day 1 part 2

--- day01/day1.py
+++ day01/day1.py
@@ -23,8 +23,35 @@ def part1(
 
     return(increased_count)
 
+def part2(
+    input_lst: list,
+) -> int:
+    """This function calculate the number of measurements that 
+    are larger than the previous measurement in a moving window of len 3.
+
+    Args:
+        input_lst (list): The puzzle input data.
+
+    Returns:
+        int: The number of times there was an increase.
+    """
+    increased_count = 0
+
+    for i in range(0, len(input_lst) - 3):
+        window1 = input_lst[(i):(i)+3]
+        window2 = input_lst[(i+1):(i+1)+3]
+
+        if sum(window1) < sum(window2):
+            increased_count = increased_count + 1
+
+    return(increased_count)
+
 puzzle_input = read_input('input1.txt')
 
 #Part 1 solution
-increased_count = part1(puzzle_input)
-print("Part 1 Solution: %s"%increased_count)
+part1_solution = part1(puzzle_input)
+print("Part 1 Solution: %s"%part1_solution)
+
+#Part 2 solution
+part2_solution = part2(puzzle_input)
+print("Part 2 Solution: %s"%part2_solution)
